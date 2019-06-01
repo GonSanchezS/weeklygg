dd4759e00089c2066b96d0aa488d8dda559c61a5
Gonzalo <gonsanchezs@gmail.com>
2019-06-01 11:29:49 +0300

    Migrate companies

--- db/migrate/20190601081545_create_companies.rb
+++ db/migrate/20190601081545_create_companies.rb
@@ -1,6 +1,12 @@
 class CreateCompanies < ActiveRecord::Migration[5.2]
   def change
     create_table :companies do |t|
+      t.string   "name"
+      t.string   "url"
+      t.text     "description"
+      t.string   "city"
+      t.string   "country"
+      t.string   "size"
 
       t.timestamps
     end
