91bebe6fc5478e6d77cf381aa9bb665e4d3795a5
Gonzalo <gonsanchezs@gmail.com>
2019-06-01 16:15:46 +0300

    add stuff

--- db/migrate/20190601122826_rename_country_to_country_id_in_companies.rb
+++
@@ -1,6 +0,0 @@
-class RenameCountryToCountryIdInCompanies < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :cities, :country
-    add_reference :cities, :country, foreign_key: true
-  end
-end
